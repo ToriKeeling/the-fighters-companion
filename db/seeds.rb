0210980a42436b395ff5c0054cfb6a60b6da91d6
ToriKeeling <tori.keeling@gmail.com>
2023-11-01 21:32:18 -0500

    added weapons seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Weapon.create(name: "Unarmed", description: "the action of fighting without weapons.", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgUt4JTwfP3e_wUVYPcDN44IOAYM5xl0Euow&usqp=CAU")
+Weapon.create(name: "Broadsword", description: "a sword with a wide blade, used primarily for cutting rather than thrusting.", image_url: "https://pics.knifecenter.com/knifecenter/united/images/UC3265_1.jpg")
+Weapon.create(name: "Rapier and Dagger", description: "a thin, light sharp-pointed sword used for thrusting and a short knife with a pointed and edged blade", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-njmLMnS_SBNLLR7RXMKdAGaLBkhllnV17w&usqp=CAU")
+Weapon.create(name: "Single Sword", description: "a sword with a hilt that is held in one hand, sometimes having only one true edge. ", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDwu1_9d0xVQTw0dpHpGPev9zZcWO7SFCtjbcMzrUn4BO37FpS88JCW6jrfbyXj-Vs2E4&usqp=CAU")
+Weapon.create(name: "Small Sword", description: "a light, tapering thrusting sword.", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcmBqCjqdxc6qtMB_iweCfGtRDZJ7g4zcxHw&usqp=CAU")
+Weapon.create(name: "Sword and Shield ", description: "a bladed weapon held in one hand and a piece of personal armour held in the other.", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhUbZ85JlUaol-eyf0Y_arEsVouvSbxu8chQ&usqp=CAU")
+Weapon.create(name: "Quarterstaff", description: "a long stout staff formerly used as a weapon and wielded with one hand in the middle and the other between the middle and the end.", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWi4PXBEaZfXrYkC3BN7HcSeKrMNH5LeBKRg&usqp=CAU")
+Weapon.create(name: "Knife", description: "an instrument composed of a blade fixed into a handle, used for cutting or as a weapon.", image_url: "https://www.ikea.com/us/en/images/products/vardagen-chefs-knife-dark-gray__0710384_pe727512_s5.jpg?f=s")
